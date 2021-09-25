be3f66f143651228ebf1c5c244ed762cbcb924f5
snxx-lppxx <snmgreenx@gmail.com>
2021-09-25 22:27:33 +0500

    Update setup.v

---
+++ setup.v
@@ -0,0 +1,12 @@
+module main
+import setuper { setup }
+
+setup(
+	name='telegram',
+	version='1.0.0',
+	repo='https://github.com/vTelegramBot/v-telegram-bot',
+	license='MIT',
+	author='snxx',
+	email='snxx.lppxx@gmail.com',
+	description='V for Vendetta'
+)
